b7eb68464e4aa60a15a0ddca963dab903d1fe639
Chris <644191901@qq.com>
2019-09-15 18:51:43 +0800

    1.0.1

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@
 * @Author: Chris
 * @Date:   2019-09-12 17:38:50
 * @Last Modified by:   Chris
-* @Last Modified time: 2019-09-14 19:29:40
+* @Last Modified time: 2019-09-15 18:50:46
 */
 alert(true);
-console.log(1);
