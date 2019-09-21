4388c2f4f8bd2f43a24965491ef9a0125139c473
Chris <644191901@qq.com>
2019-09-21 20:18:15 +0800

    :)

--- js/index.js
+++ js/index.js
@@ -2,6 +2,6 @@
 * @Author: Chris
 * @Date:   2019-09-12 17:38:50
 * @Last Modified by:   Chris
-* @Last Modified time: 2019-09-19 17:01:31
+* @Last Modified time: 2019-09-21 20:17:35
 */
-console.log()
+console.log();
