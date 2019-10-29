43f5d44a672d3a9a535ce07e17cbb1637ae4a8eb
Chris <644191901@qq.com>
2019-10-29 13:03:57 +0800

    分类管理-更新手机分类名称和排序

--- kmall/admin/src/api/config.js
+++ kmall/admin/src/api/config.js
@@ -2,17 +2,20 @@
 * @Author: Chris
 * @Date:   2019-10-28 14:39:49
 * @Last Modified by:   Chris
-* @Last Modified time: 2019-10-29 10:50:58
+* @Last Modified time: 2019-10-29 12:59:16
 */
 export const SERVER = 'http://127.0.0.1:3000'
 
 
 export const API_CONFIG = {
-	login: 				['/sessions/users','post'],
-	logout: 			['/sessions/users','delete'],
-	getCounts: 			['/counts','get'],
-	getUserList: 		['/users/list','get'],
-	addCategories: 		['/categories','post'],
-	getlevelCategories: ['/categories/levelCategories','get'],
-	getCategoriesList: 	['/categories/list','get'],
+	login: 							['/sessions/users','post'],
+	logout: 						['/sessions/users','delete'],
+	getCounts: 						['/counts','get'],
+	getUserList: 					['/users/list','get'],
+	addCategories: 					['/categories','post'],
+	getlevelCategories: 			['/categories/levelCategories','get'],
+	getCategoriesList: 				['/categories/list','get'],
+	updateCategoriesName: 			['/categories/name','put'],
+	updateCategoriesMobileName: 	['/categories/mobileName','put'],
+	updateCategoriesOrder: 			['/categories/order','put'],
 }
--- kmall/admin/src/pages/category/store/actionCreator.js
+++ kmall/admin/src/pages/category/store/actionCreator.js
@@ -1,15 +1,17 @@
 import api from 'api'
 import { message } from 'antd'
+
 import * as types  from './actionTypes.js'
+
 import { saveUsername } from 'util'
 
 const getPageReqestStartAction = ()=>({
     type:types.PAGE_REQEST_START,
 })
-
-const getPageReqestDonetAction = ()=>({
+const getPageReqestDoneAction = ()=>({
     type:types.PAGE_REQEST_DONE,
 })
+
 const getSetPageAction = (payload)=>({
     type:types.SET_PAGE,
     payload
@@ -19,6 +21,7 @@ const setCategoriesAction = (payload)=>({
     type:types.SET_CATEGORIES,
     payload
 })
+
 export const getAddAction = (values)=>{
     return (dispatch,getState)=>{
         api.addCategories(values)
@@ -29,10 +32,11 @@ export const getAddAction = (values)=>{
             }else{
                 message.error(result.message)
             }
+            
         })
         .catch(err=>{
             message.error('网络错误,请稍后再试')
-        })    
+        })              
     }
 }
 export const getLevelCategoriesAction = ()=>{
@@ -42,16 +46,14 @@ export const getLevelCategoriesAction = ()=>{
         })
         .then(result=>{
             if(result.code == 0){
-                console.log(result.data)
                 dispatch(setCategoriesAction(result.data))
             }
         })
         .catch(err=>{
             message.error('网络错误,请稍后再试')
-        })    
+        })              
     }
 }
-
 export const getPageAction = (page)=>{
     return (dispatch,getState)=>{
         dispatch(getPageReqestStartAction())
@@ -60,20 +62,103 @@ export const getPageAction = (page)=>{
         })
         .then(result=>{
             if(result.code == 0){
-               dispatch(getSetPageAction(result.data))
+                dispatch(getSetPageAction(result.data))
             }else{
                 message.error('获取分类数据失败,请稍后再试')
             }
         })
         .catch(err=>{
             message.error('网络错误,请稍后再试')
-        })    
+        })
         .finally(()=>{
             dispatch(getPageReqestDoneAction())
-        })
+        })                 
     }
 }
-
+export const getUpdateNameAction = (id,newName)=>{
+    return (dispatch,getState)=>{
+        const page = getState().get('category').get('current')
+        api.updateCategoriesName({
+            id:id,
+            name:newName,
+            page:page
+        })
+        .then(result=>{
+            if(result.code == 0){
+                message.success('更新分类名称成功')
+                dispatch(getSetPageAction(result.data))
+            }else{
+                message.error(result.message)
+            }
+        })
+        .catch(err=>{
+            message.error('网络错误,请稍后再试')
+        })               
+    }
+}
+export const getUpdateMobileNameAction = (id,newMobileName)=>{
+    return (dispatch,getState)=>{
+        const page = getState().get('category').get('current')
+        api.updateCategoriesMobileName({
+            id:id,
+            mobileName:newMobileName,
+            page:page
+        })
+        .then(result=>{
+            if(result.code == 0){
+                message.success('更新手机分类名称成功')
+                dispatch(getSetPageAction(result.data))
+            }else{
+                message.error(result.message)
+            }
+        })
+        .catch(err=>{
+            message.error('网络错误,请稍后再试')
+        })               
+    }
+}
+export const getUpdateOrderAction = (id,newOrder)=>{
+    return (dispatch,getState)=>{
+        const page = getState().get('category').get('current')
+        api.updateCategoriesOrder({
+            id:id,
+            order:newOrder,
+            page:page
+        })
+        .then(result=>{
+            if(result.code == 0){
+                message.success('更新排序成功')
+                dispatch(getSetPageAction(result.data))
+            }else{
+                message.error(result.message)
+            }
+        })
+        .catch(err=>{
+            message.error('网络错误,请稍后再试')
+        })               
+    }
+}
+export const getUpdateUpdateIsShowAction = (id,newIsShow)=>{
+    return (dispatch,getState)=>{
+        const page = getState().get('category').get('current')
+        api.updateCategoriesIsShow({
+            id:id,
+            isShow:newIsShow,
+            page:page
+        })
+        .then(result=>{
+            if(result.code == 0){
+                message.success('更新显示隐藏成功')
+                dispatch(getSetPageAction(result.data))
+            }else{
+                message.error(result.message)
+            }
+        })
+        .catch(err=>{
+            message.error('网络错误,请稍后再试')
+        })               
+    }
+}
 
 
 
