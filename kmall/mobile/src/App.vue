d89b230b4151cca7f0e24c813059b774e12e0f3a
Chris <644191901@qq.com>
2019-10-30 15:30:52 +0800

    商城移动端-rem配置

--- kmall/mobile/src/App.vue
+++ kmall/mobile/src/App.vue
@@ -1,34 +1,31 @@
 <!--模版-->
 <template>
     <div id="app">
-        <!--3.使用组件-->
-        <Header />
-        <List  />
-        <Footer />
+        <p>
+            hello
+        </p>
     </div>
 </template>
 
 <!--逻辑-->
 <script>
     //1.引入组件
-    import Header from './pages/Header.vue'
-    import List from './pages/List.vue'
-    import Footer from './pages/Footer.vue'
+    
     export default {
         name:'App',
         //2.注册组件
         components:{
-            Header,
-            List,
-            Footer,
+            
         }
     }
 </script>
 
 <!--样式-->
-<style>
+<style lang="less">
     #app{
-        width: 600px;
-        margin: 100px auto;
+       p{
+        color:red;
+        .rem('padding',10px)
+       }
     }
 </style>
--- kmall/mobile/vue.config.js
+++ kmall/mobile/vue.config.js
@@ -2,10 +2,20 @@
 * @Author: Chris
 * @Date:   2019-10-29 17:00:05
 * @Last Modified by:   Chris
-* @Last Modified time: 2019-10-29 17:01:04
+* @Last Modified time: 2019-10-30 15:29:03
 */
+const path = require('path')
+
 module.exports = {
-	devServer:{
-		port:3003
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'less',
+      patterns: [
+      	path.resolve(__dirname, './src/assets/less/index.less')
+      ]
+    }
+},
+devServer:{
+	port:3003
 	}
 }
