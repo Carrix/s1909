c5ff45bb49b2594d7b65828443ceb49777bc53e8
Chris <644191901@qq.com>
2019-11-06 10:47:44 +0800

    11-06

--- kmall/admin/src/pages/user/store/actionTypes.js
+++
@@ -1,7 +0,0 @@
-
-
-
-
-export const SET_COUNT = "user@set_page"
-export const PAGE_REQRST_START = "user@page_reqest_start"
-export const PAGE_REQRST_DONE = "user@page_reqest_done"
--- kmall/mobile/src/api/config.js
+++
@@ -1,9 +0,0 @@
-
-var API_CONFIG = {
-    getPositionAds:              ['/ads/positionAds','get'],
-    getFloors:                   ['/floors','get'] 
-}
-
-module.exports = {
-    API_CONFIG
-}
