8da08be47e2571b8f4ab62b767f3ea7309b3dbe0
Chris <644191901@qq.com>
2020-01-29 22:11:54 +0800

    add mini

---
+++ mini/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from "react-dom"
+const createApp=() =>{
+    return <h1>Welcome React!!!</h1>
+} 
+const app = <h1>welcome React!!!</h1>
+ReactDOM.render{
+    app,
+    document.querySelector('root')
+}
