5e2ed9b609d7d9f71e7a866babe1d80d60749d2d
Chris <644191901@qq.com>
2020-01-29 22:31:54 +0800

    :)

--- mini/src/index.js
+++ mini/src/index.js
@@ -1,10 +1,25 @@
 import React from 'react'
-import ReactDOM from "react-dom"
-const createApp=() =>{
-    return <h1>Welcome React!!!</h1>
-} 
-const app = <h1>welcome React!!!</h1>
-ReactDOM.render{
-    app,
-    document.querySelector('root')
+import ReactDOM from 'react-dom'
+
+// const createApp=(props) =>{
+//     return (
+//         <div>
+//             <h1>Welcome {props.title}!!!</h1>
+//         </div>
+//     )
+// }
+// const app = createApp({
+//     title:'React 16.10'
+// })
+const App = (props) => {
+    return (
+        <div>
+            <h1>Welcome {props.title}!!!</h1>
+            <p>优秀{props.title}</p>
+        </div>
+    )
 }
+ReactDOM.render(
+    <App title="222" />,
+    document.querySelector('#root')
+)
