9241165978cd45c9d95d69112d5b0aa8c1a6ee3b
Chris <644191901@qq.com>
2020-01-30 21:03:11 +0800

    JSX原理

--- mini/src/index.js
+++ mini/src/index.js
@@ -1,25 +1,84 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// const createApp=(props) =>{
-//     return (
-//         <div>
-//             <h1>Welcome {props.title}!!!</h1>
-//         </div>
-//     )
+// class App extends Component {
+//     render() {
+//         return (
+//             <div className="app" id="appRoot">
+//                 <h1 className="title">JSX原理</h1>
+//                 <p>类组件是继承React.Component</p>
+//             </div>
+//         )
+//     }
 // }
-// const app = createApp({
-//     title:'React 16.10'
-// })
-const App = (props) => {
-    return (
-        <div>
-            <h1>Welcome {props.title}!!!</h1>
-            <p>优秀{props.title}</p>
-        </div>
-    )
+
+// const appVDom = {
+//     tag:'div',
+//     attrs: {
+//         className:'app',
+//         id:'appRoot'
+//     },
+//     children:[
+//         {
+//             tag:'h1',
+//             attrs:{
+//                 className:'title'
+//             },
+//             children:['JSX原理']
+//         },
+//         {
+//             tag:'p',
+//             attrs:null,
+//             children:['类组件是继承React.Component的']
+//         }
+//     ]
+// }
+
+
+// class App extends Component {
+//     render (){
+//         return (
+//             <div className="app" id="appRoot">
+//                 <h1 className="title">JSX原理</h1>
+//                 <p>类组件是继承React.Component的</p>
+//             </div>
+//         )
+//     }
+// }
+
+
+class App extends Component{
+	render() {
+		return (
+			//React.createElement是一个方法，用于创建元素，可以有很多的参数，但是前两个是固定的
+			//第一个可以理解为标签名
+			//第二个可以理解为标签的属性
+			//剩下的，继续写更多的子元素
+			//React.createElement(type,[props],[...children])
+			React.createElement(
+				'div',
+				{
+					className:'app',
+					id:'appRoot'
+				},
+				React.createElement(
+					'h1',
+					{
+						className:'title'
+					},
+					'jsx原理'
+				),
+				React.createElement(
+					'p',
+					null,
+					'类组件是继承React.Component的'
+				),
+			)
+		)
+	}
 }
-ReactDOM.render(
-    <App title="222" />,
-    document.querySelector('#root')
+
+render(
+	<App />,
+	document.querySelector('#root')
 )
