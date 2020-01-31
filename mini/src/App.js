c31224650e742755ee889cef961f38d284a713a3
Chris <644191901@qq.com>
2020-01-31 16:32:16 +0800

    组件的内部数据-state

--- mini/src/App.js
+++ mini/src/App.js
@@ -1,22 +1,32 @@
 import React, { Component,Fragment } from 'react'
 import {
-  TodoHeader,
-  TodoInput,
-  TodoList
+	TodoHeader,
+	TodoInput,
+	TodoList
 } from './components'
 
 export default class App extends Component{
-  render(){
-    return (
-      <Fragment>
-        <TodoHeader 
-          desc="daydayup"
-        >
-          待办事项列表
-        </TodoHeader>
-        <TodoInput btnText="ADD" />
-        <TodoList />
-      </Fragment>
-    )
-  }
+	// state = {
+	// 	title:'待办事项列表'
+	// }
+	constructor(){
+		super()
+		this.state = {
+			title:'待办事项列表',
+			desc:'今日事今日毕'
+		}
+	}
+	render(){
+		return (
+			<Fragment>
+				<TodoHeader 
+					desc={this.state.desc}
+				>
+					{this.state.title}
+				</TodoHeader>
+				<TodoInput />
+				<TodoList />
+			</Fragment>
+		)
+	}
 }
